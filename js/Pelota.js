78af8cdb9ec0efdfbdc32258c3cb2653f9ea4952
Daniel Segovia <segoviadaniel2@gmail.com>
2017-09-30 21:18:27 -0300

    Atributos y métodos a la clase Pelota + Loop principal del juego

--- js/Pelota.js
+++ js/Pelota.js
@@ -1,6 +1,51 @@
-
 class Pelota {
 
-	
+	constructor(x, y, radio, color, desplazamientoX, desplazamientoY){
+		this.x = x;
+		this.y = y;
+		this.radio = radio;
+		this.color = color;
+		this.desplazamientoX = +1.5;
+		this.desplazamientoY = +1.5;
+	}
+
+	dibujarse(){
+		contexto.beginPath();
+		contexto.arc(this.x, this.y, this.radio, 0, Math.PI*2);
+		contexto.fillStyle = this.color;
+		contexto.fill();
+		contexto.closePath();
+	}
+
+	moverse(){
+		this.x = this.x + this.desplazamientoX;
+		this.y = this.y + this.desplazamientoY
+	}
+
+	detectarColision(anchoCanvas, altoCanvas){
+
+		//Colisiones en los bordes izquierdo y derecho
+		if(this.x + this.radio >= anchoCanvas){
+			console.log('SE SALIO, UPS!');
+			this.desplazamientoX*=-1;
+		}
+		if(this.x - this.radio <= 0){
+			this.desplazamientoX*=-1;
+		}
+
+		//Colision en los bordes superior e inferior
+
+		if(this.y + this.radio >= altoCanvas){
+			console.log('Colision abajo');
+			this.desplazamientoY*=-1;
+		}
+
+		if(this.y - this.radio <= 0){
+			console.log('Colision arriba');
+			this.desplazamientoY*=-1;
+		}
+
+	}
+
 }
 
--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,27 @@
-
-
 let canvas = document.getElementById('miCanvas');
 let contexto = canvas.getContext('2d');
 
-console.dir(contexto);
+const altura = canvas.height;
+const anchura = canvas.width;
+
+let pelotaEnJuego;
+
+let cargarObjetos = () => {
+
+	pelotaEnJuego = new Pelota(anchura/2, altura/2, 25, 'blue');
+	pelotaEnJuego.dibujarse();
+};
+
+let dibujar = () => {
+	//Limpiar pantalla
+	contexto.clearRect(0, 0, anchura, altura);
+	pelotaEnJuego.dibujarse();
+	pelotaEnJuego.detectarColision(anchura, altura);
+	pelotaEnJuego.moverse();
+};
+
+
+window.addEventListener('load', cargarObjetos);
+
+window.setInterval( dibujar, 10);
 
